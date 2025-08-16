adf0975eec8ec4e6d4455a55d255a66b1ccc9064
jose7Manuel <jose.gomez@uab.edu.bo>
2025-08-16 18:26:44 -0400

    configuracion de las migraciones

---
+++ backend/database/migrations/2025_08_16_221719_create_servicios_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('servicios', function (Blueprint $table) {
+        $table->id();
+        $table->string('nombre');
+        $table->decimal('precio', 8, 2);
+        $table->string('duracion');
+        $table->timestamps();
+    });
+}
+
+};
